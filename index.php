21c26369fc809783bc7425349958492b6cb17124
Diantarii <127836891+Diantarii@users.noreply.github.com>
2023-03-14 17:07:23 +0800

    Sesi 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML Sesi 1</title>
+</head>
+<body>
+    
+    <h1 style="text-align: center;">Belajar Web Progamming<?="<sup>80</sup>"?></h1>
+
+    <script>
+        document.write("<h3>Menggunakan Javascript</h3");
+    </script>
+
+<?php
+    echo "<h3>Menggunakan Script PHP</h3>";
+?>
+
+</body>
+</html>
